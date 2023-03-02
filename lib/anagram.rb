f44acfb0752f1a4ff889302578e742f08c50d211
+ <+>
2023-03-03 01:04:40 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+      @word = word
+    end
+
+    def match(words)
+      words.filter { |wrd| wrd.split("").sort == word.split("").sort }
+    end
+  end
+
+  listen = Anagram.new("listen")
+  puts listen.word
+  puts listen.match(%w{enlists google inlets banana})
